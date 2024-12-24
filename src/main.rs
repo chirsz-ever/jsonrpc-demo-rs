e29406c9b9a0f8a1386626ca67937a74f6f94c1c
chirsz-ever <chirsz@foxmail.com>
2024-12-24 21:05:57 +0800

    support batch and notifacation

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,13 @@ fn handle_client(stream: TcpStream) -> std::io::Result<()> {
         if line.is_empty() {
             continue;
         }
-        match Json::parse_with_trailing_whitespace(&line) {
-            Err(e) => {
-                response_error(writer, -32700, Json::Null, &format!("{e}"))?;
-                continue;
-            }
+        let is_notify = match Json::parse_with_trailing_whitespace(&line) {
+            Err(_) => response_error(writer, -32700, Json::Null, "Parse error").map(|_| false)?,
             Ok(j) => handle_json(writer, &j)?,
         };
+        if !is_notify {
+            write!(writer, "\n")?;
+        }
     }
     Ok(())
 }
@@ -40,7 +40,7 @@ fn response_error(
     id: Json,
     message: &str,
 ) -> std::io::Result<()> {
-    writeln!(
+    write!(
         writer,
         "{}",
         Json::Object(vec![
@@ -62,33 +62,70 @@ fn response_invalid_request(writer: &mut dyn Write) -> std::io::Result<()> {
     response_error(writer, -32600, Json::Null, "Invalid Request")
 }
 
-fn handle_json(writer: &mut dyn Write, j: &Json) -> std::io::Result<()> {
+// return whether the request is notification
+fn handle_json(writer: &mut dyn Write, j: &Json) -> std::io::Result<bool> {
     match j {
-        Json::Object(kvs) => {
-            let Some(Json::String(jsonrpc_version)) = get(&kvs, "jsonrpc") else {
-                return response_invalid_request(writer);
-            };
-            if jsonrpc_version != "2.0" {
-                return response_invalid_request(writer);
+        Json::Object(kvs) => handle_single_json(writer, &kvs),
+        Json::Array(js) => {
+            if js.is_empty() {
+                return response_invalid_request(writer).map(|_| false);
             }
-
-            let Some(id) = get(&kvs, "id") else {
-                return response_invalid_request(writer);
-            };
-            match id {
-                Json::Null | Json::Number(_) | Json::String(_) => (),
-                _ => return response_invalid_request(writer),
+            let mut res = Vec::<u8>::new();
+            let mut is_first = true;
+            for j in js {
+                let mut res_j = Vec::<u8>::new();
+                let is_notify = match j {
+                    Json::Object(kvs) => handle_single_json(&mut res_j, &kvs)?,
+                    _ => response_invalid_request(&mut res_j).map(|_| false)?,
+                };
+                if !is_notify {
+                    if is_first {
+                        is_first = false;
+                        write!(res, "[")?;
+                    } else {
+                        write!(res, ",")?;
+                    }
+                    res.write_all(&res_j)?;
+                }
             }
-
-            let Some(Json::String(method)) = get(&kvs, "method") else {
-                return response_error(writer, -32601, id.clone(), "Method not found");
-            };
-            match method.as_str() {
-                "add" => handle_method_add(writer, kvs, id),
-                _ => response_error(writer, -32601, id.clone(), "Method not found"),
+            if !is_first {
+                writer.write_all(&res)?;
+                write!(writer, "]")?;
+                Ok(false)
+            } else {
+                Ok(true)
             }
         }
-        _ => response_invalid_request(writer),
+        _ => response_invalid_request(writer).map(|_| false),
+    }
+}
+
+// return whether the request is notification
+fn handle_single_json(writer: &mut dyn Write, kvs: &[(String, Json)]) -> std::io::Result<bool> {
+    let Some(Json::String(jsonrpc_version)) = get(&kvs, "jsonrpc") else {
+        return response_invalid_request(writer).map(|_| false);
+    };
+    if jsonrpc_version != "2.0" {
+        return response_invalid_request(writer).map(|_| false);
+    }
+
+    // simple ignore notification
+    let Some(id) = get(&kvs, "id") else {
+        return Ok(true);
+    };
+    match id {
+        Json::Number(_) | Json::String(_) => (),
+        Json::Null => return Ok(true),
+        _ => return response_invalid_request(writer).map(|_| false),
+    }
+
+    let Some(Json::String(method)) = get(&kvs, "method") else {
+        return response_error(writer, -32601, id.clone(), "Method not found").map(|_| false);
+    };
+    match method.as_str() {
+        "add" => handle_method_add(writer, kvs, id).map(|_| false),
+        "subtract" => handle_method_subtract(writer, kvs, id).map(|_| false),
+        _ => response_error(writer, -32601, id.clone(), "Method not found").map(|_| false),
     }
 }
 
@@ -113,21 +150,48 @@ fn handle_method_add(
     let Some(Json::Array(args)) = get(&kvs, "params") else {
         return response_invalid_parameters(writer, id);
     };
-    if !args.iter().all(|a| matches!(a, Json::Number(_))) {
-        return response_invalid_parameters(writer, id);
-    }
-    let result = args.iter().fold(0.0, |acc, x| {
-        if let Json::Number(x) = x {
-            acc + *x
+    let mut result = 0.0;
+    for arg in args {
+        if let Json::Number(x) = arg {
+            result += x;
         } else {
-            acc
+            return response_invalid_parameters(writer, id);
         }
-    });
+    }
     let res = Json::Object(vec![
         ("jsonrpc".into(), "2.0".into()),
         ("id".into(), id.clone()),
         ("result".into(), Json::Number(result)),
     ])
     .stringify();
-    writeln!(writer, "{res}")
+    write!(writer, "{res}")
+}
+
+fn handle_method_subtract(
+    writer: &mut dyn Write,
+    kvs: &[(String, Json)],
+    id: &Json,
+) -> std::io::Result<()> {
+    let Some(Json::Array(args)) = get(&kvs, "params") else {
+        return response_invalid_parameters(writer, id);
+    };
+    if args.len() != 2 {
+        return response_invalid_parameters(writer, id);
+    }
+    let mut arg_nums = [0.0; 2];
+    for (i, arg) in args.iter().enumerate() {
+        if let Json::Number(x) = arg {
+            arg_nums[i] = *x;
+        } else {
+            return response_invalid_parameters(writer, id);
+        }
+    }
+    let result = arg_nums[0] - arg_nums[1];
+    let res = Json::Object(vec![
+        ("jsonrpc".into(), "2.0".into()),
+        ("id".into(), id.clone()),
+        ("result".into(), Json::Number(result)),
+    ])
+    .stringify();
+    write!(writer, "{res}")
 }
